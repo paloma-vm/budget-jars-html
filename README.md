21d5edb2e93abeff9200832698952df54c21c9a7
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-03-21 00:43:10 -0600

    some progress

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Budget Jars
 
 Spring Intensive March 2023
+
+An app for those who want to use the jar system for budgeting and reducing debt, but don't want to have to use actual cash.  This digital version of the jar system is easy to use.
--- main.js
+++ main.js
@@ -8,7 +8,18 @@ const jars = []
 // const jarsContainer = document.querySelector('#jars')
 const jarList = document.getElementById('jars')
 
+// get all jars in jarList
+// const allJars = jarList.getElementsByClassName('jar')
+const allJars = jarList.getElementsByClassName('btn')
+
+
+
 const options = document.getElementById('options')
+const mathAmount = Number(document.getElementById('amt').value)
+console.log(mathAmount)
+
+const depositButton = document.getElementById('depositButton')
+const spendButton = document.getElementById('spendButton')
 
 
 
@@ -17,13 +28,25 @@ function makeJar(label, amount) {
   jars.push(jar)
 }
 
+// function highlightDiv() {
+//   // document.getElementById('jar-0').style.backgroundColor = 'cyan';
+//   const elejar = document.getElementById(`'jar-${i}'`);
+//   elejar.classList.toggle('highlight');
+// }
+
 
 // help from mood-shop assignment
 function showJars() {
   let jarsDisplay = ''
   for (let i = 0; i < jars.length; i += 1) {
     const { label, amount } = jars[i]
-
+// as buttons
+    // jarsDisplay += `<button class='btn'><div class='jar' id='jar-${i}'>
+    // <img src='empty-jar.png'>
+    // <h3 class=display-label>${label}</h3>
+    // <h3 class=display-amount>$${amount}</h3>
+    // </div></button>`
+  // NOT as buttons
     jarsDisplay += `<div class='jar' id='jar-${i}'>
     <img src='empty-jar.png'>
     <h3 class=display-label>${label}</h3>
@@ -48,10 +71,47 @@ function jarSelect() {
 }
 
 function highlightDiv() {
-  // document.getElementById('jar-0').style.backgroundColor = 'cyan';
   const jar0 = document.getElementById('jar-0');
   jar0.classList.toggle('highlight');
 }
+
+// function makeActive() {
+//   const element = document.getElementById(`'jar-${i}'`);
+//   // element.classList.add('active');
+//   element.classList.toggle('active');
+
+// }
+
+function findSelected() {
+  
+}
+
+function doMath() {
+  // let jarChoices = ''
+  for (let i = 0; i < jars.length; i += 1) {
+    const { label, amount } = jars[i]
+
+    // options.getElementsByTagName("option");
+    for (let i = 0; i < options.length; i += 1) {
+      const val = options[i].getAttribute('value'); // could maybe use class here
+      if (val == label) {
+        // do math here
+        console.log(jars[i])
+        console.log(jars[i].amount)
+        jars[i].deposit(mathAmount)
+        console.log(jars[i].amount)
+        
+      }
+    }
+
+   
+  }
+  // options.innerHTML = jarChoices
+}
+// get deposits and add to jar amount
+depositButton.addEventListener('click', (e) => {
+  
+})
 
 
 	
@@ -65,6 +125,27 @@ makeJar('Clothes/gifts', 100)
 makeJar('Everything else', 100)
 const gameFund = new Jar('games', 75)
 jars.push(gameFund)
+
+// tried this: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_active_element2
+// loop through the jar div and add the active class to the currently selected div
+for (let i = 0; i < allJars.length; i += 1) {
+  allJars[i].addEventListener('click', function() {
+    const current = document.getElementsByClassName('active');
+
+    // if there is no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(' active', '');
+      // current[0].className = current[0].classList.toggle('highlight')
+      // current[0].className = current[0].classList.toggle(' active')
+
+    }
+
+    // add the active class to the currently selected div, if you are switching which div is active
+    this.className += ' active';
+    // this.classList.toggle('highlight');
+
+  });
+}
 
 // ------------------------------------------------
 // manage the money in the jars
@@ -91,6 +172,9 @@ console.log(jars[5].amount) //105
 document.getElementById('jar-0').addEventListener('click', highlightDiv);
 
 jarSelect()
+console.log(options)
+console.log(options.length)
+doMath()
 
 
 
